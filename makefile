1c5a4257d2efe327ff51a8f83279c63777f2d7ac
Xillendo <olivier.pelgrin@gmail.com>
2016-03-07 19:26:50 +0100

    adding audioserver

--- makefile
+++ makefile
@@ -1,15 +1,15 @@
 CC 		= gcc
 CFLAGS	= -Wall -g -std=c11
 
-SRC		= src/lecteur.c include/audio.c
+SRC		= src/audioserver.c include/audio.c
 
-OBJ 	= obj/lecteur.o obj/audio.o
+OBJ 	= obj/audioserver.o obj/audio.o
 	
-main: $(OBJ)
+audioserver: $(OBJ)
 	$(CC) -o ./bin/$@ $(OBJ)
 	
-obj/lecteur.o: src/lecteur.c
-	$(CC) $(CFLAGS) -c -o obj/lecteur.o src/lecteur.c	
+obj/audioserver.o: src/audioserver.c
+	$(CC) $(CFLAGS) -c -o obj/audioserver.o src/audioserver.c	
 	
 obj/audio.o: include/audio.c
 	$(CC) $(CFLAGS) -c -o obj/audio.o include/audio.c
@@ -20,15 +20,19 @@ depend:
 clean:
 	rm ./obj/*.o
 	rm ./bin/*
-	
+
 # DO NOT DELETE
 
-src/lecteur.o: /usr/include/stdio.h /usr/include/features.h
-src/lecteur.o: /usr/include/stdc-predef.h /usr/include/libio.h
-src/lecteur.o: /usr/include/_G_config.h /usr/include/wchar.h
-src/lecteur.o: /usr/include/stdlib.h /usr/include/alloca.h
-src/lecteur.o: /usr/include/unistd.h /usr/include/getopt.h
-src/lecteur.o: /usr/include/strings.h /usr/include/xlocale.h include/audio.h
+src/audioserver.o: /usr/include/stdio.h /usr/include/features.h
+src/audioserver.o: /usr/include/stdc-predef.h /usr/include/libio.h
+src/audioserver.o: /usr/include/_G_config.h /usr/include/wchar.h
+src/audioserver.o: /usr/include/stdlib.h /usr/include/alloca.h
+src/audioserver.o: /usr/include/unistd.h /usr/include/getopt.h
+src/audioserver.o: /usr/include/netinet/in.h /usr/include/stdint.h
+src/audioserver.o: /usr/include/endian.h /usr/include/arpa/inet.h
+src/audioserver.o: /usr/include/string.h /usr/include/xlocale.h
+src/audioserver.o: /usr/include/fcntl.h /usr/include/time.h
+src/audioserver.o: /usr/include/signal.h include/audio.h
 include/audio.o: /usr/include/stdlib.h /usr/include/features.h
 include/audio.o: /usr/include/stdc-predef.h /usr/include/alloca.h
 include/audio.o: /usr/include/stdio.h /usr/include/libio.h
--- src/audioserver.c
+++ src/audioserver.c
@@ -1,4 +1,6 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -6,13 +8,19 @@
 #include <string.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <signal.h>
+#include "../include/audio.h"
 
 #define BUFFER_SIZE 256
 
 void exec_server(int sfd);
-char* get_next_buffer(int sfd, int n);
+int get_next_buffer(int fd, char* buffer);
+void exit_server(char* buf, int sfd, int ffd);
 
-int send(char* buf, int seqnum);
+void sig_exit(int signum);
+ 
+int send_buf(int sfd, char* buf, struct sockaddr_in dest);
+int send_metadata(int sfd, struct sockaddr_in dest, int sample_rate, int sample_size, int channels);
 
 
 int get_file(char* name);
@@ -24,7 +32,7 @@ int main(){
 	socket_fd = socket(AF_INET, SOCK_DGRAM, 0);
 	if(socket_fd < 0){
 		perror("Error socket");
-		exit(
+		exit(1);
 	}
 	
 	maddr.sin_family = AF_INET;
@@ -32,7 +40,7 @@ int main(){
 	maddr.sin_addr.s_addr = htonl(INADDR_ANY);
 	
 	int ret_bind;
-	ret_bind = bind(socket_fd, (struct sockaddr *) &addr, sizeof(struct sockaddr_in));
+	ret_bind = bind(socket_fd, (struct sockaddr *) &maddr, sizeof(struct sockaddr_in));
 	if(ret_bind < 0){
 		perror("Error bind");
 		exit(1);
@@ -45,25 +53,24 @@ int main(){
 
 void exec_server(int fd){
 	//protocol commands
-	char get = "GET";
-	char next = "NEXT";
-	char last = "LAST";
+	char* get = "GET";
+	char* next = "NEXT";
+	char* last = "LAST";
 	
-	int last_seq;
-	int current_seq;
+	char* end = "END";
 	
-	int fd;
+	int file_fd;
 	
-	int stop = 1;
+	//int stop = 0;
 	
 	char* buffer = (char*) malloc(BUFFER_SIZE * sizeof(char));
 	
 	char buf[32];
-	int buf_len = sizeof(pckt);
-	int from_len = sizeof(struct sockaddr_in);
+	int buf_len = sizeof(buf);
+	unsigned int from_len = sizeof(struct sockaddr_in);
 	struct sockaddr_in from;
 	
-	while(stop){
+	while(1){
 		int ret = recvfrom(fd, buf, buf_len, 0, (struct sockaddr*) &from, &from_len);
 		if(ret < 0){
 			perror("Error recvfrom");
@@ -73,27 +80,49 @@ void exec_server(int fd){
 		char *cmd;
 		char *arg;
 		//split of the received buffer
-		cmd = strtok(buf, ' '); //1st split is the command
-		arg = strtok(NULL, ' '); //2nd  split is the file (if GET)
+		cmd = strtok(buf, " "); //1st split is the command
+		arg = strtok(NULL, " "); //2nd  split is the file (if GET)
+		
 		
 		//exec command
 		if(cmd != NULL) {
-			if(strncmp(cmd, next, strlen(next)) == 0){
-				last_seq = current_seq;
-				current_seq++;
-				buffer = get_next_buffer(fd, current_seq);
-				stop = send(buff, current_seq);
+			// Commande NEXT
+			if(strncmp(cmd, next, strlen(next)) == 0){ 
+				int ret_get_nxt = get_next_buffer(file_fd, buffer);
+				int r = send_buf(fd, buffer, from);
+				if(r < 0){
+					exit(1);
+				}
+				if(ret_get_nxt == 1){
+					int send_return;
+					send_return = sendto(fd, end, sizeof(end), 0, (struct sockaddr*) &from, sizeof(struct sockaddr_in));
+					if(send_return < 0){
+						perror("Error send END message");
+						exit(1);
+					}
+				}
+			// Commande LAST	
 			} else if(strncmp(cmd, last, strlen(last)) == 0){
-				stop = send(buff, current_seq);
+				int r = send_buf(fd, buffer, from);
+				if(r < 0){
+					exit(1);
+				}
+			// Commande GET	
 			} else if(strncmp(cmd, get, strlen(get)) == 0){
 				if(arg != NULL){
-					fd = open(arg, O_RDONLY);	
-					if(fd < 0){
+					int sample_rate;
+					int sample_size;
+					int channels;
+					file_fd = aud_readinit(arg, &sample_rate, &sample_size, &channels);	
+					if(file_fd < 0){
 						perror("Error open file");
 						exit(1);
 					}
-					current_seq = 0;
-					last_seq = 0;
+					int ret_send_meta;
+					ret_send_meta = send_metadata(fd, from, sample_rate, sample_size, channels);
+					if(ret_send_meta < 0){
+						exit(1);
+					}					
 				}
 			} else {
 				printf("Error : unknown protocol command\n");
@@ -104,11 +133,65 @@ void exec_server(int fd){
 			exit(1);
 		}
 	}
-	free(buffer);
+	//free(buffer);
 }
-			
-		
-		
+
+int send_buf(int sfd, char *buf, struct sockaddr_in dest){
+	int send_return;
+	send_return = sendto(sfd, buf, sizeof(buf), 0, (struct sockaddr*) &dest, sizeof(struct sockaddr_in));
+	if(send_return < 0){
+		printf("Send_buf : Failed to send packet\n");
+		return -1;
+	} else {
+		printf("packet send\n");
+		return 0;
+	}
+}
+
+int get_next_buffer(int fd, char* buf){
+	int read_size = read(fd, buf, BUFFER_SIZE);
+	if(read_size == -1){
+		perror("Error read file");
+		return -1;
+	} else if(read_size != BUFFER_SIZE){
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+int send_metadata(int sfd, struct sockaddr_in dest, int sample_rate, int sample_size, int channels){
+	int buf[3];
+	buf[0] = sample_rate;
+	buf[1] = sample_size;
+	buf[2] = channels;
+	int send_return;
+	send_return = sendto(sfd, buf, sizeof(buf), 0, (struct sockaddr*) &dest, sizeof(struct sockaddr_in));
+	if(send_return < 0){
+		printf("send_metadata : Failed to send packet\n");
+		return -1;
+	} else {
+		printf("send_metadata : Packet send\n");
+		return 0;
+	}
+}
+	
+/*
+void exit_server(char* buf, int sfd, int ffd){
+	printf("Closing active file...");
+	int r1 = close(ffd);
+	if(r1 < 0){
+		perror("Can't close file file descriptor !");
+	}
+	printf("Closing socket...\n");
+	int r2 = close(sfd);
+	if(r2 < 0){
+		perror("Can't close socket file descriptor !");
+	}
+	free(buf);
+	exit(0);
+}*/
+
 		
 		
 		
